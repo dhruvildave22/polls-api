0a654b98b3d664ffcabd8a3435eef1841263eefb
Dhruvil Dave <dhruvildave22@gmail.com>
2020-04-22 15:33:24 +0530

    update done

--- pollsapi/settings.py
+++ pollsapi/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dhruvildave22@gmail.com'
-EMAIL_HOST_PASSWORD = 'Punita@081997'
+EMAIL_HOST_USER = 'user@gmail.com'
+EMAIL_HOST_PASSWORD = 'Abcd@1234'
